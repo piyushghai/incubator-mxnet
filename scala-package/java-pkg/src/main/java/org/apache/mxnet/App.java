4417c5948e1355687641bb4834a2a69fad467af1
Piyush Ghai <ghai.8@osu.edu>
2018-09-26 17:04:53 -0700

    Added a hello world java project to run through Maven pipelinve piggybacking on Scala Builds

---
+++ scala-package/java-pkg/src/main/java/org/apache/mxnet/App.java
@@ -0,0 +1,15 @@
+package org.apache.mxnet;
+
+import java.util.ArrayList;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
