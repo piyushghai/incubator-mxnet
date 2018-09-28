15e7f46da309a08e6f5d52fa961fb168823652d7
Piyush Ghai <ghai.8@osu.edu>
2018-09-28 13:42:35 -0700

    Reverting back to the older methods of running example

--- scala-package/examples/src/test/scala/org/apache/mxnetexamples/infer/imageclassifier/ImageClassifierExampleSuite.scala
+++ scala-package/examples/src/test/scala/org/apache/mxnetexamples/infer/imageclassifier/ImageClassifierExampleSuite.scala
@@ -63,8 +63,7 @@ class ImageClassifierExampleSuite extends FunSuite with BeforeAndAfterAll {
       context = Context.gpu()
     }
 
-    val output = ImageClassifierExample.runInferenceOnSingleImage(
-      new ImageClassifierExample(modelDirPath + "resnet-18", inputImagePath, inputImageDir), context)
+    val output = ImageClassifierExample.runInferenceOnSingleImage(modelDirPath + "resnet-18", inputImagePath, context)
 
     val outputList = ImageClassifierExample.runInferenceOnBatchOfImage(modelDirPath + "resnet-18",
         inputImageDir, context)
